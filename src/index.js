fb30d64602acbe923ea3aaa1e191a36a83dba02e
Vitaliy Shapiro <shapirovet@gmail.com>
2019-12-06 22:39:36 +0300

    1.6

--- src/index.js
+++ src/index.js
@@ -10,7 +10,9 @@
  */
 function delayPromise(seconds) {
     let promise = new Promise((resolve) => {
-        setTimeout(() => resolve, seconds);
+        setTimeout(() => {
+          resolve()
+        }, seconds);
     });
 
     return promise;
