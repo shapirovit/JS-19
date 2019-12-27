3246d9e19e8fe027ac4bda93e55115677773a58a
Vitaliy Shapiro <shapirovet@gmail.com>
2019-12-27 13:29:14 +0300

    project-2 1.0.1

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@
 
 window.onload = map(); */
 
-
+/* 
 var app = require('express')();
 var http = require('http').createServer(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.sendFile('C:\Users\A8-7050\Desktop\JS-project\index.html');
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
 });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
-});
+}); */
