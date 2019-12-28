7a15ca5522e266ff84ec5438b35d2fc43cbe86fc
Vitaliy Shapiro <shapirovet@gmail.com>
2019-12-29 01:13:11 +0300

    project-2 2.1.0

--- src/index.js
+++ src/index.js
@@ -1,20 +1,3 @@
-/* import { mapInit as map } from './js/ymaps';
+import { Init as initChat } from './js/chat';
 
-window.onload = map(); */
-
-
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
-app.get('/', function(req, res){
-  res.sendFile('C:/Users/A8-7050/Desktop/JS-project/index.hbs');
-});
-
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
-
-http.listen(8080, function(){
-  console.log('listening on *:8080');
-});
+initChat();
--- src/js/chat.js
+++ src/js/chat.js
@@ -1,234 +1,187 @@
-var socket = io();
+function Init() {
+    // let socket = io();
+    const socket = io('http://localhost');  // ??? или let socket = io.connect('http://localhost:3000/'); 
 
-function submit() {
-    var socket = io();
-    let input = document.querySelector('#m');
-    let button = document.querySelector('button');
-    button.addEventListener('click', e => {
-        e.preventDefault();
-        console.log('input.value=', input.value);
-        socket.emit('chat message', input.value);
-        input.value = '';
-        return false;
-    });
-    socket.on('chat message', msg => {
-        let message = document.querySelector('#message');
-        let li = document.createElement('li');
-        li.textContent = msg;
-        message.append(li);
-    });
-};
-submit();
+    function authorization() {
+        let formBlock = document.querySelector('.form-block');
+        let name = document.querySelector('#name');
+        let login = document.querySelector('#login');
+        let buttonAuth = document.querySelector('.form-block-button');
+        let avatarText = document.querySelector('.block-avatar-name');
+        let blockAvatar = document.querySelector('.block-avatar');
+        let blockAvatarFoto = document.querySelector('.block-avatar-foto');
+        let blockParticipants = document.querySelector('.block-participants');
+        let message = document.querySelector('.block-right-bottom');
 
-
-
-
-
-
-
-
-
-
-
-
-
-/* // import { createPlacemark as placemarkAdd } from '../js/geo-elements';
-
-function mapInit() {
-    ymaps.ready(() => {
-        let historyAll = {};
-        let value;
-        let eventClick;      
-    
-        // 1. Создаем карту
-        let myMap = new ymaps.Map('map', {
-            center: [55.650625, 37.62708],
-            zoom: 10,
-            controls: ['zoomControl']
-        }, {
-            yandexMapDisablePoiInteractivity: true
-        });
-
-        // 2. Задаем обработчик клика по нашей карте
-        myMap.events.add('click', function (e) {
-            let coords = e.get('coords');
-            let position = e.get('position');
-            let myGeoCoder = ymaps.geocode(coords);
-            
-            value = coords;
-    
-            myGeoCoder.then(res => {
-                let obj = new Object();
-
-                obj.coords = coords;
-                obj.address = res.geoObjects.get(0).properties.get('text');
-                openForm(obj, position, e);
-            });    
-        });
-    
-        // 3. Создаем шаблон кластера
-        let customItemContentLayout = ymaps.templateLayoutFactory.createClass(
-            // Флаг "raw" означает, что данные вставляют "как есть" без экранирования html.
-            '<h2 class=ballon_header>{{ properties.place|raw }}</h2>' +
-            '<a href="" id=mAdress class=ballon_body>{{ properties.address|raw }}</a></br></br>' +
-            '<div class=ballon_body>{{ properties.feedback|raw }}</div></br>' +
-            '<div class=ballon_footer>{{ properties.date|raw }}</div>', {
-    
-                build: function () {                
-                    customItemContentLayout.superclass.build.call(this);
-
-                    let mAdress = document.querySelector('#mAdress');
-
-                    mAdress.addEventListener('click', this.onAddress.bind(this));
-                },
-
-                clear: function () {
-                    let mAdress = document.querySelector('#mAdress');
-
-                    mAdress.addEventListener('click', this.onAddress);                
-                    customItemContentLayout.superclass.clear.call(this);
-                },
-        
-                onAddress: function (e) {                
-                    let coords = this.getData().properties._data.coords;
-                    let address = this.getData().properties._data.address;            
-                    let position = [e.clientX, e.clientY];
-                    let obj = new Object();
-
-                    e.preventDefault();
-                    value = coords;                
-                    obj.coords = coords;
-                    obj.address = address;
-                    openForm(obj, position, e);
-                    this.events.fire('userclose');
-                }    
-            }
-        );
-
-        // 4. Создаем кластер
-        let clusterer = new ymaps.Clusterer({
-            clusterDisableClickZoom: true,
-            clusterOpenBalloonOnClick: true,
-            hideIconOnBalloonOpen: false,
-            clusterBalloonContentLayout: 'cluster#balloonCarousel',
-            clusterBalloonItemContentLayout: customItemContentLayout,
-            clusterBalloonPanelMaxMapArea: 0,
-            clusterBalloonContentLayoutWidth: 300,
-            clusterBalloonContentLayoutHeight: 200,
-            clusterBalloonPagerSize: 5
-        });
-
-        // 5. Функция создания метки на карте по координатам
-        function createPlacemark(coords) {
-            let placemark = new ymaps.Placemark(coords);
-    
-            placemark.events.add('click', (e) => {
-                let position = e.get('position');
-                let myGeoCoder = ymaps.geocode(coords);
-                
-                value = coords;        
-                myGeoCoder.then(res => {
-                    let obj = new Object();
-
-                    obj.coords = coords;
-                    obj.address = res.geoObjects.get(0).properties.get('text');
-                    openForm(obj, position, e);
-                });
-            });
-
-            return placemark;
-        }
-    
-        // 6. Функция открытия формы
-        function openForm(obj, position, e) {
-            let hist;
-            let addressTextModal = document.querySelector('#adressText');
-            let historyFeedbackModal = document.querySelector('#historyFeedback');
-            let modalForm = document.querySelector('#formFeedback');
-            let addButton = document.querySelector('#addButton');
-            let close = document.querySelector('.close');
-            let nameInput = document.querySelector('#nameInput');
-            let placeInput = document.querySelector('#placeInput');
-            let feedbackInput = document.querySelector('#feedbackInput');
-    
-            if (!historyAll[value]) {
-                hist = 'Отзывов пока нет...';
-            } else hist = historyAll[value];            
-
-            addressTextModal.textContent = obj.address;    
-            historyFeedbackModal.innerHTML = hist;
-    
-            if (modalForm.classList.contains('modalActive')) {
-                addButton.removeEventListener('click', eventClick);
+        buttonAuth.addEventListener('click', e => {                
+            e.preventDefault();
+            if (name.value === '' || login.value === '') {
+                alert('Введите своё имя и логин!');
             } else {
-                modalForm.classList.remove('modalForm');
-                modalForm.classList.add('modalActive');
-            }
-    
-            modalForm.style = `top: ${position[1]}px ; left: ${position[0]}px;`;    
-            eventClick = clickAdd.bind(e);
-            addButton.addEventListener('click', eventClick);            
-            close.addEventListener('click', () => {                
-                nameInput.value = '';
-                placeInput.value = '';
-                feedbackInput.value = '';
-                modalForm.classList.remove('modalActive');
-                modalForm.classList.add('modalForm');
-                addButton.removeEventListener('click', eventClick);
-            });
-
-            // Функция обработки клика по кнопке
-            function clickAdd() {
-                let date = new Date();
-                let printDate = date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
-                let printTime = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2) + ':' + ('0' + date.getSeconds()).slice(-2);
-    
-                if (nameInput.value === '') {
-                    alert('Введите пожалуйста своё имя!');
-                } else {
-                    if (placeInput.value === '') {
-                        alert('Введите пожалуйста название места!');
-                    } else {
-                        if (feedbackInput.value === '') {
-                            alert('Введите пожалуйста свой отзыв!');
-                        } else {
-                            let myPlacemark = createPlacemark(obj.coords);                            
-                            let messageFeedback = ['<div class="historyFeedback__box">',
-                                `<strong>${nameInput.value}</strong> `,
-                                `${placeInput.value}`,
-                                ` <time>${printDate} ${printTime}</time>`,
-                                `<span>${feedbackInput.value}</span>`,
-                                '</div>'].join('');
-                
-                            if (historyAll[value]) {
-                                historyAll[value] += messageFeedback;
-                            } else {
-                                historyAll[value] = messageFeedback;    
-                            }
-                            historyFeedbackModal.innerHTML = historyAll[value];
-                            myPlacemark.properties
-                                .set({
-                                    name: nameInput.value,
-                                    place: placeInput.value,
-                                    address: obj.address,
-                                    coords: obj.coords,
-                                    feedback: feedbackInput.value,
-                                    date: `${printDate} ${printTime}`
-                                });                            
-                            clusterer.add(myPlacemark);
-                            myMap.geoObjects.add(clusterer);    
-                            nameInput.value = '';
-                            placeInput.value = '';
-                            feedbackInput.value = '';
-                        }
-                    }
+                let person = {
+                    login: login.value,
+                    name: name.value,
+                    id: socket.id,
+                    foto: 'http://www.techportal.ru/upload/nophoto.jpg'
                 }
+                socket.emit('authorization', person);
+                formBlock.classList.add('hidden');
+                blockAvatar.classList.remove('hidden');
+                blockParticipants.classList.remove('hidden');
+                message.classList.remove('hidden');
+                blockAvatarFoto.src = person.foto;
+                avatarText.textContent = name.value;
+
+                                  
             }
+        });
+    }    
+
+    function addMessage(e) {
+        e.preventDefault();
+        let input = document.querySelector('.form-message-input');
+        let date = new Date();
+        let printTime = ('0' + date.getHours()).slice(-2) + ':' + ('0' + date.getMinutes()).slice(-2);
+        console.log('input.value=', input.value);                
+        socket.emit('chat message', /* socket.id, */ printTime, input.value);
+        input.value = '';
+        /* return false; */
+    }
+
+    function renderMessage(foto, name, time, msg) {
+        let rightTop = document.querySelector('.block-right-top');
+        let blockMessage = document.createElement('div');
+        let fotoblock = document.createElement('div');
+        let imgBlock = document.createElement('img');
+        let messageTop = document.createElement('div');
+        let nameBlock = document.createElement('div');
+        let timeBlock = document.createElement('time');
+        let messageBottom = document.createElement('div');
+        let messageBlock = document.createElement('div');
+
+        blockMessage.classList.add('block-right-message');
+        fotoblock.classList.add('block-right-message-fotoblock');
+        imgBlock.classList.add('block-right-message-foto');
+        messageTop.classList.add('block-right-message-top');
+        nameBlock.classList.add('block-right-message-top-name');
+        timeBlock.classList.add('block-right-message-top-time');
+        messageBottom.classList.add('block-right-message-bottom');
+        messageBlock.classList.add('block-right-message-bottom-text');
+
+        imgBlock.src = foto;
+        nameBlock.textContent = name;
+        timeBlock.textContent = ` ${time}`;
+        messageBlock.textContent = msg;
+
+        fotoblock.append(imgBlock);
+        messageTop.append(nameBlock);
+        messageTop.append(timeBlock);
+        messageBottom.append(messageBlock);
+
+        blockMessage.append(fotoblock);
+        blockMessage.append(messageTop);
+        blockMessage.append(messageBottom);
+
+        rightTop.append(blockMessage);
+    }
+
+    function submit() {
+        let input = document.querySelector('.form-message-input');
+        let button = document.querySelector('.form-message-button');
+        console.log('socket=', socket);
+        button.addEventListener('click', addMessage);
+        // input.addEventListener('change', addMessage);
+        socket.on('chat message', (foto, name, time, msg) => {
+            renderMessage(foto, name, time, msg);
+            /* let message = document.querySelector('.block-right-top');
+            let div = document.createElement('div');
+            div.classList.add('block-right-message-bottom-text');
+            div.textContent = msg;
+            message.append(div);
+            console.log('socket=', socket); */
+        });
+    };
+
+    function renderParticipants(allClients) {
+        console.log('allClients=', allClients);
+            let blockParticipants = document.querySelector('.block-participants');                
+            let participants = document.createElement('p');
+            blockParticipants.innerHTML = '';
+            blockParticipants.append(participants);
+
+            participants.classList.add('block-participants-text-count');                
+            participants.textContent = `Участники(${allClients.length})`;
+            for (let i = 0; i < allClients.length; i++) {
+                let clientName = document.createElement('p');
+                clientName.classList.add('block-participants-text-person');
+                clientName.textContent = allClients[i];
+                blockParticipants.append(clientName);                    
+            }
+    };
+
+    function countParticipants() {
+
+        socket.on('plus', allClients => {
+            renderParticipants(allClients);
+        });
+        socket.on('minus', allClients => {                
+            renderParticipants(allClients);
+        });
+    }
+
+    function fotoLoad() {
+        let fotoChoice = document.querySelector('.block-avatar-foto-choice');
+        fotoChoice.addEventListener('click', e => {
+            e.preventDefault();
+            let formLoadFoto = document.querySelector('.form-loadFoto');
+            formLoadFoto.classList.remove('hidden');
+        })
+
+    }
+
+    function formFoto() {
+        let buttonCancel = document.querySelector('.form-loadFoto-inner-button-cancel');
+        let buttonLoad = document.querySelector('.form-loadFoto-inner-button-load');
+        let placeDrop = document.querySelector('.form-loadFoto-inner-placeDrop');
+
+        buttonCancel.addEventListener('click', e => {
+            e.preventDefault();
+            let formLoadFoto = document.querySelector('.form-loadFoto');
+            let placeDropFoto = document.querySelector('.form-loadFoto-inner-placeDrop-foto');
+
+            formLoadFoto.classList.add('hidden');
+            placeDropFoto.src = '';                
+        });
+
+        buttonLoad.addEventListener('click', e => {
+            e.preventDefault();
+
+
+        });
+
+        placeDrop.addEventListener('click', e => {
+            e.preventDefault();
+
+
+
+        })
+
+
+
+
+
+    }
+
+
+
     
-        }
-    });
+    authorization();
+    submit();
+    countParticipants();
+    fotoLoad();
+    formFoto();
 }
 
 export {
-    mapInit
-} */
+    Init
+};
